3a5db835732c41e1586003955d2e8bec533a5a35
Julian Kohlman <juliankohlman@gmail.com>
2018-03-31 22:58:28 -0400

    es6 aside section

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,97 +1,15 @@
 'use strict';
 
-/*eslint-disable */
+// arrow functions
 
-// MUST RUN ON CLI
-
-// babel src/app.js --out-file=public/scripts/app.js --presets=env,react
-// babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
-
-// then run live server public
-
-console.log('App.js is running');
-
-var app = {
-  title: 'Indecision App',
-  subtitle: 'An app for getting things done.',
-  options: ['Hex', 'Dec', 'Bin']
+var square = function square(x) {
+  return Math.pow(x, 2);
 };
 
-// JSX - Javascript XML
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    'p',
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.options.length ? 'Here are your options' : 'No options'
-  ),
-  React.createElement(
-    'ol',
-    null,
-    React.createElement(
-      'li',
-      null,
-      'Item one'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'Item two'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'Item three'
-    )
-  )
-);
+console.log(square(7));
 
-var user = {
-  name: 'Julian',
-  age: 33,
-  location: 'Burlingame'
+var getFirstName = function getFirstName(fullName) {
+  return fullName.split(' ')[0];
 };
 
-// booleans get ignored (do not get rendered)
-var getLocation = function getLocation(location) {
-  if (location) return React.createElement(
-    'p',
-    null,
-    'location: ',
-    location
-  );
-};
-
-// true && truthy value => render truthy value
-// false && truthy value => false(a boolean) gets ignored (not rendered)
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    user.name ? user.name : 'Anonymous'
-  ),
-  user.age && user.age >= 18 && React.createElement(
-    'p',
-    null,
-    'Age: ',
-    user.age
-  ),
-  getLocation(user.location)
-);
-
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
+console.log(getFirstName('Linus Torvald'));
---
+++ indecision-app/src/playground/arrow-functions.js
@@ -0,0 +1,9 @@
+// arrow functions
+
+const square = x => x ** 2;
+
+console.log(square(7));
+
+const getFirstName = fullName => fullName.split(' ')[0];
+
+console.log(getFirstName('Linus Torvald'));
