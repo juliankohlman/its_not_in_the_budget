116ada258bd678b059a0437bdf8a77e848c1597b
Julian Kohlman <juliankohlman@gmail.com>
2018-04-06 22:47:16 -0400

    binding onClick methods

--- indecision-app/src/playground/counterExample.js
+++ indecision-app/src/playground/counterExample.js
@@ -1,19 +1,38 @@
+/* eslint semi: "off" */
+/* eslint no-console: "off" */
 class Counter extends React.Component {
-  static propTypes = {
-    name: React.PropTypes.string,
-  };
-
   constructor(props) {
     super(props);
+    this.addOne = this.addOne.bind(this);
+    this.minusOne = this.minusOne.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  addOne() {
+    console.log('addOne');
+  };
+
+  minusOne() {
+    console.log('minusOne');
+  }
+
+  reset() {
+    console.log('reset');
   }
 
   render() {
     return (
-      <div>Hello</div>
+      <div>
+        <h1> Counter Example </h1>
+        <button onClick={this.addOne}> +1 </button>
+        <button onClick={this.minusOne}> -1 </button>
+        <button onClick={this.reset}> reset </button>
+      </div>
     );
   }
 }
 
+ReactDOM.render(<Counter />, document.getElementById('app'));
 // let count = 0;
 
 // const addOne = () => {
@@ -31,7 +50,6 @@ class Counter extends React.Component {
 //   renderCounterApp();
 // }
 
-
 // const renderCounterApp = () => {
 //   const templateTwo = (
 //     <div>
@@ -41,8 +59,6 @@ class Counter extends React.Component {
 //       <button onClick={reset}>Reset</button>
 //     </div>
 //   );
-
-  ReactDOM.render(Counter, appRoot);
 // };
 
 // renderCounterApp();
