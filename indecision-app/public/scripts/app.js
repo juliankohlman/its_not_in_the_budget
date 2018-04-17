d697e31d9ed6695c62a6505fd3a15eaa2d11b86f
Julian Kohlman <juliankohlman@gmail.com>
2018-04-16 21:11:48 -0400

    Using default exports

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,6 +1,23 @@
 'use strict';
 
-require('./utils');
+var _utils = require('./utils');
+
+var _utils2 = _interopRequireDefault(_utils);
+
+var _person = require('./person');
+
+var _person2 = _interopRequireDefault(_person);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// relative file path
 
 console.log('app.js is running!!');
-console.log(square(4));
+
+console.log((0, _utils.square)(4));
+console.log((0, _utils.add)(12, 26));
+console.log((0, _utils2.default)(11, 7));
+
+console.log((0, _person.canDrink)(33));
+console.log((0, _person.isAdult)(12));
+console.log((0, _person2.default)(60));
