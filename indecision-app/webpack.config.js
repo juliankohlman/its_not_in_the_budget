93c1cefd4c6e07482389b8a5ed443fa534ee239e
Julian Kohlman <juliankohlman@gmail.com>
2018-04-18 10:06:23 -0400

    Add getCurrentYear hbs helper

--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -7,13 +7,17 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        // loader (defining how a file gets used)
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
   }
 };
-
-// loader (defining how a file gets used)
-
