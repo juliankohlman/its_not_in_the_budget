29ec8cbbc9b04fb7ef771ede883696b4c51813d8
Julian Kohlman <juliankohlman@gmail.com>
2018-04-23 20:26:10 -0400

    container styling

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
-
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 // props => stateless functional component
 // this.props => class based component
