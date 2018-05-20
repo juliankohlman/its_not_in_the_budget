10099b8edf7da2b1f45c1e3f1c61d747e4ef744e
Julian Kohlman <juliankohlman@gmail.com>
2018-05-20 19:39:40 -0400

    Linking between pages

---
+++ boilerplate V1/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+// props => stateless functional component
+// this.props => class based component
+ReactDOM.render(<p>Some boilerplate</p>, document.getElementById('app'));
--- expensify-app /src/app.js
+++ expensify-app /src/app.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const ExpenseDashBoard = () => <div>My ExpenseDashBoard component</div>;
+
+const AddExpense = () => <div>My AddExpense component</div>;
+
+const EditExpense = () => (<div><p>My EditExpense component</p></div>);
+
+const HelpExpense = () => (<div><p>Stateless help page component</p></div>);
+
+const NotFoundPage = () => (<div><p>404! This is not the page you are looking for</p></div>);
+
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={ExpenseDashBoard} exact={true} />
+      <Route path="/create" component={AddExpense} />
+      <Route path="/edit" component={EditExpense} />
+      <Route path="/help" component={HelpExpense} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </BrowserRouter>
+);
 // props => stateless functional component
 // this.props => class based component
-ReactDOM.render(<p>Some boilerplate</p>, document.getElementById('app'));
+ReactDOM.render(routes, document.getElementById('app'));
