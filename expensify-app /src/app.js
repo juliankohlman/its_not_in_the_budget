db7cb0289932ae9d272f7572122aa3ddc3183fcd
Julian Kohlman <juliankohlman@gmail.com>
2018-05-20 20:04:44 -0400

    React Router: link/navlink

--- expensify-app /src/app.js
+++ expensify-app /src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, NavLink, Link } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -12,18 +12,34 @@ const EditExpense = () => (<div><p>My EditExpense component</p></div>);
 
 const HelpExpense = () => (<div><p>Stateless help page component</p></div>);
 
-const NotFoundPage = () => (<div><p>404! This is not the page you are looking for</p></div>);
+const NotFoundPage = () => (
+  <div>
+    404! - <Link to="/">Home page</Link>
+  </div>
+);
 
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <li><NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink></li>
+    <li><NavLink to="/create" activeClassName="is-active">Create Expense</NavLink></li>
+    <li><NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink></li>
+    <li><NavLink to="/help" activeClassName="is-active">Help Page</NavLink></li>
+  </header>
+);
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashBoard} exact={true} />
-      <Route path="/create" component={AddExpense} />
-      <Route path="/edit" component={EditExpense} />
-      <Route path="/help" component={HelpExpense} />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashBoard} exact={true} />
+        <Route path="/create" component={AddExpense} />
+        <Route path="/edit" component={EditExpense} />
+        <Route path="/help" component={HelpExpense} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 // props => stateless functional component
