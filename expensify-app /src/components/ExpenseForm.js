91845c5f175082482dc90811ac571ee39deb698d
Julian Kohlman <juliankohlman@gmail.com>
2018-08-25 11:13:45 -0400

    Add momentjs and react-dates libs

--- expensify-app /src/components/ExpenseForm.js
+++ expensify-app /src/components/ExpenseForm.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+const now = moment();
+console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends Component {
 	state = {
 		description: '',
-		note: ''
+		note: '',
+		amount: '',
+		createdAt: moment(),
+		calendarFocused: false
+	};
+
+	onFocusChange = ({ focused }) => {
+		this.setState(() => {
+			calendarFocused: focused;
+		});
+	};
+
+	onDateChange = createdAt => {
+		this.setState(() => {
+			createdAt;
+		});
 	};
 
 	onDescriptionChange = e => {
@@ -19,6 +41,14 @@ export default class ExpenseForm extends Component {
 			note
 		}));
 	};
+
+	onAmountChange = e => {
+		const amount = e.target.value;
+		if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+			this.setState(() => ({ amount }));
+		}
+	};
+
 	render() {
 		return (
 			<div>
@@ -30,7 +60,18 @@ export default class ExpenseForm extends Component {
 						value={this.state.description}
 						onChange={this.onDescriptionChange}
 					/>
-					<input type="number" placeholder="Amount" />
+					<input
+						value={this.state.amount}
+						type="text"
+						placeholder="Amount"
+						onChange={this.onAmountChange}
+					/>
+					<SingleDatePicker
+						date={this.state.createdAt}
+						onDateChange={this.onDateChange}
+						focused={this.state.calendarFocused}
+						onFocusChange={this.onFocusChange}
+					/>
 					<textarea
 						value={this.state.note}
 						onChange={this.onNoteChange}
