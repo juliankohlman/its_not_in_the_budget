3cb497100deba061f54cdbe35bbc5fc908f1a322
Julian Kohlman <juliankohlman@gmail.com>
2018-08-31 17:18:01 -0400

    expense amount formatting

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
 	<div>
 		<Link to={`/edit/${id}`}>
 			<h3>Description: {description}</h3>
 		</Link>
-		<p>Amount: ${amount}</p>
+		<p>Amount: {numeral(amount / 100).format('$0,0.00')}</p>
 		<p>Expense created: {moment(createdAt).format('MMMM Do, YYYY')}</p>
 
 		<hr />
