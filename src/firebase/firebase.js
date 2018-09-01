6fede1aa94d34d4a438f63db6cd4a3c5f2df9e90
Julian Kohlman <juliankohlman@gmail.com>
2018-09-01 08:00:33 -0400

    firebase subscriptions

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,30 +12,48 @@ const config = {
 firebase.initializeApp(config);
 const database = firebase.database();
 
-database
-	.ref()
-	.set({
-		name: 'J. Kohlman',
-		hobby: 'Daily Fantasy Sports',
-		dreamJob: 'Cinematographer',
-		location: {
-			city: 'Westminster'
-		}
-	})
-	.then(() => {
-		console.log('Data saved');
-	})
-	.catch(e => {
-		console.log('This failed', e);
-	});
-
-database.ref().update({
-	name: 'Julian J. Kohlman',
-	hobby: 'Reading',
-	location: {
-		city: 'Seattle'
-	}
+database.ref().on('value', snapshot => {
+	console.log(snapshot.val());
 });
+
+setTimeout(() => {
+	database.ref('InterstellarCredits').set(2000);
+}, 3500);
+
+// database
+// 	.ref('ship') // for specific data add inside ref() call
+// 	.once('value')
+// 	.then(data => {
+// 		console.log(data.val());
+// 	})
+// 	.catch(error => {
+// 		console.log('Oops', error);
+// 	});
+
+// database
+// 	.ref()
+// 	.set({
+// 		name: 'Jango Fett',
+// 		hobby: 'Horansi',
+// 		dreamJob: 'Sith Lord',
+// 		homeworld: 'Mandalore',
+// 		InterstellarCredits: 1500,
+// 		ship: {
+// 			class: 'Patrol Craft',
+// 			hyperDrive_Rating: 3.0
+// 		}
+// 	})
+// 	.then(() => {
+// 		console.log('Data saved');
+// 	})
+// 	.catch(e => {
+// 		console.log('This failed', e);
+// 	});
+
+// database.ref().update({
+// 	dreamJob: 'Smuggler',
+// 	homeworld: 'Coruscant'
+// });
 
 // database.ref('location/city').set(null)
 
