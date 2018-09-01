04e61cc32e685f16d5ca7ae4a6d9e48c9ac38801
Julian Kohlman <juliankohlman@gmail.com>
2018-08-31 22:38:37 -0400

    firebase promises

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,11 +1,28 @@
 const promise = new Promise((resolve, reject) => {
 	setTimeout(() => {
-		resolve('This is my resolved data');
+		let n = 5;
+		n <= 2
+			? resolve('This is my resolved data')
+			: reject('Error: Opps something went wrong');
 	}, 2000);
 });
 console.log('resolving data....');
 
-promise.then(data => {
-	console.log(data);
-});
-console.log('data resolved....');
+promise.then(
+	data => {
+		console.log('1', data);
+		console.log('data resolved....');
+	}, // without .catch() example
+	error => {
+		console.log('error:', error);
+	}
+);
+
+promise
+	.then(data => {
+		console.log('2', data);
+		console.log('data resolved....');
+	})
+	.catch(error => {
+		console.log('error:', error);
+	});
