11087ed61d70e11e2ccf681d40c178aedc322d88
Julian Kohlman <juliankohlman@gmail.com>
2018-09-03 12:47:04 -0400

    Test database, and env variable setup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 // Initialize Firebase
 const config = {
-	apiKey: 'AIzaSyCV6YNROyjGEOy9bwbTpmU7iwkbIfgxZ98',
-	authDomain: 'expenses-cac5e.firebaseapp.com',
-	databaseURL: 'https://expenses-cac5e.firebaseio.com',
-	projectId: 'expenses-cac5e',
-	storageBucket: 'expenses-cac5e.appspot.com',
-	messagingSenderId: '766054544742'
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID
 };
 
 firebase.initializeApp(config);
