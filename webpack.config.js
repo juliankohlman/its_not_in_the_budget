96e66b808f90a3dc90c47d572bf57ce5d89dd5b6
Julian Kohlman <juliankohlman@gmail.com>
2018-09-03 12:14:57 -0400

    Add firebase test DB

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = env => {
 	const isProduction = env === 'production';
