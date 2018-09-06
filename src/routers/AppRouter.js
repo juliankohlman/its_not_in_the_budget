6e51ce5a9d66591e60520693e7711e1cea4f9987
Julian Kohlman <juliankohlman@gmail.com>
2018-09-06 11:29:14 -0400

    Protected route component

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import { Router, Switch, Route, Link, NavLink } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-import Header from '../components/Header';
 import ExpenseDashBoard from '../components/ExpenseDashBoard';
 import CreateExpense from '../components/CreateExpense';
 import EditExpense from '../components/EditExpense';
 import Help from '../components/Help';
 import NotFound from '../components/NotFound';
 import Login from '../components/LoginPage';
+import ProtectedRoute from './ProtectedRoute';
 
 export const history = createHistory();
 
 const AppRouter = () => (
 	<Router history={history}>
 		<div>
-			<Header />
 			<Switch>
 				<Route path="/" component={Login} exact={true} />
-				<Route
+				<ProtectedRoute
 					path="/dashboard"
 					component={ExpenseDashBoard}
-					exact={true}
 				/>
-				<Route path="/create" component={CreateExpense} />
-				<Route path="/edit/:id" component={EditExpense} />
+				<ProtectedRoute path="/create" component={CreateExpense} />
+				<ProtectedRoute path="/edit/:id" component={EditExpense} />
 				<Route path="/help" component={Help} />
 				<Route component={NotFound} />
 			</Switch>
