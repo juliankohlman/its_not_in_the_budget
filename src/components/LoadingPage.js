8cd86d6ad82a0cef87c6e3a81d2080446392fe2b
Julian Kohlman <juliankohlman@gmail.com>
2018-09-10 11:20:31 -0400

    loading page done with fixed bottom page margin

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingPage = () => (
+	<div className="loader-container">
+		<div className="loader">
+			<img className="loader-image" src="/images/loader2.gif" />
+		</div>
+	</div>
+);
+
+export default LoadingPage;
