86925fb929a1551d7f7014b1457423e12b4897e6
Julian Kohlman <juliankohlman@gmail.com>
2018-12-04 00:02:09 -0500

    Add header logo image Update font-family with montserrat and poor-story fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,13 @@ const Header = ({ startLogout }) => (
 		<div className="content">
 			<div className="header-content">
 				<Link className="header-title" to="/dashboard">
-					<h1>It's not in the budget!</h1>
+					{/* <h1>It's not in the budget!</h1> */}
 					{/* ADD LOGO HERE and replace h1 text */}
-					{/* <img src="https://via.placeholder.com/150" alt="" /> */}
+					<img
+						className="logo-img"
+						src="../images/twitter_header_photo_1.png"
+						alt=""
+					/>
 				</Link>
 
 				<button className="button button--link" onClick={startLogout}>
